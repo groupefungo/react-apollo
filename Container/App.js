973201f8e392edee6deeadfe09d9a1cdc6ce9d78
nicolas pineault <npineault@fungo.ca>
2020-02-27 15:29:37 -0500

    refactoring project struct

--- Container/App.js
+++ Container/App.js
@@ -9,14 +9,12 @@ var _react = _interopRequireDefault(require("react"));
 
 var _reactApollo = require("@groupefungo/react-apollo.core");
 
-var _reactApollo2 = require("@groupefungo/react-apollo.theme");
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(_ref) {
   var children = _ref.children,
       t = _ref.t;
-  return _react["default"].createElement(_reactApollo2.ThemeContextProvider, {
+  return _react["default"].createElement(_reactApollo.ThemeContextProvider, {
     t: t
   }, _react["default"].createElement(_reactApollo.AppContextProvider, null, children));
 };
