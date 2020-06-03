34d59b72e8da18b1e803b31b69de3fe4eda0f55e
Nicolas Pineault <nicolas.pineault@gmail.com>
2020-06-03 15:29:14 -0400

    simple table refactoring useRouteParam

--- packages/core/ui/SimpleTable.js
+++ packages/core/ui/SimpleTable.js
@@ -9,13 +9,22 @@ var _react = _interopRequireDefault(require("react"));
 
 var _UseContext = _interopRequireDefault(require("./UseContext"));
 
+var _useAppContext2 = require("../app/UseContext/useAppContext");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(_ref) {
-  var headers = _ref.headers,
-      rows = _ref.rows,
+  var rows = _ref.rows,
+      translationNamespace = _ref.translationNamespace,
       columns = _ref.columns,
-      handleRowClick = _ref.handleRowClick;
+      rowObjectKey = _ref.rowObjectKey,
+      rowClicked = _ref.rowClicked;
+
+  var _useAppContext = (0, _useAppContext2.useAppContext)(),
+      useTranslate = _useAppContext.useTranslate;
+
+  var _useTranslate = useTranslate(),
+      t = _useTranslate.t;
 
   var _useUiContext = (0, _UseContext["default"])(),
       makeStyles = _useUiContext.makeStyles;
@@ -41,12 +50,12 @@ var _default = function _default(_ref) {
       var name = column.name,
           component = column.component;
       return _react["default"].createElement(TableCell, {
-        key: "".concat(column, "_").concat(object.id)
+        key: "col-".concat(column, "_").concat(object[rowObjectKey])
       }, component(object[name]));
     }
 
     return _react["default"].createElement(TableCell, {
-      key: "".concat(column, "_").concat(object.id)
+      key: "col-".concat(column, "_").concat(object[rowObjectKey])
     }, object[column]);
   };
 
@@ -55,12 +64,16 @@ var _default = function _default(_ref) {
   }, _react["default"].createElement(Table, {
     className: classes.table,
     "aria-label": "simple table"
-  }, _react["default"].createElement(TableHead, null, _react["default"].createElement(TableRow, null, headers.map(function (header) {
-    return _react["default"].createElement(TableCell, null, header.label);
+  }, _react["default"].createElement(TableHead, null, _react["default"].createElement(TableRow, {
+    key: "headers"
+  }, columns.map(function (cname) {
+    return _react["default"].createElement(TableCell, {
+      key: "header-".concat(cname)
+    }, t("".concat(translationNamespace, ".").concat(cname)));
   }))), _react["default"].createElement(TableBody, null, rows.map(function (object) {
     return _react["default"].createElement(TableRow, {
-      key: object.id,
-      onClick: handleRowClick(object.id)
+      key: "row-".concat(object[rowObjectKey]),
+      onClick: rowClicked(object)
     }, columns.map(function (column) {
       return TableCellComponent(object, column);
     }));
